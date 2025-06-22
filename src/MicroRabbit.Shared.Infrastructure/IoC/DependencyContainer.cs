fb5087b80ee721408b6bc6d420f0101ca4a31511
juanarayafonsec <juan-jose95@hotmail.com>
2025-06-22 09:10:12 +0200

    Add dependency injection support and event bus setup
    - Added `Microsoft.Extensions.DependencyInjection` package reference.
    - Introduced `DependencyContainer` class with `RegisterServices` method.
    - Registered `IEventBus` as a singleton using `RabbitMQBus`.
    - Configured MediatR to register services from the current assembly.

---
+++ src/MicroRabbit.Shared.Infrastructure/IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Shared.Domain.Bus;
+using MicroRabbit.Shared.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Shared.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Register RabbitMQ Bus
+            //RabbitMQ connections are expensive to create and the class also keep states
+            services.AddSingleton<IEventBus, RabbitMQBus>();
+            // Register MediatR
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyContainer).Assembly));
+            // Register other dependencies as needed
+            // Example: services.AddTransient<IYourService, YourServiceImplementation>();
+        }
+    }
+}
